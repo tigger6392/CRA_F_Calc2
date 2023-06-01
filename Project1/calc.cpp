cbd8c5a63115dc5024918c597d25d63873bbf1e5
JK <jksdasom@gmail.com>
2023-06-01 10:12:16 +0900

    Add function & test(Gop)

--- Project1/calc.cpp
+++ Project1/calc.cpp
@@ -1,4 +1,8 @@
 class Cal
 {
 public:
+	int getGop(int a, int b)
+	{
+		return (a*b);
+	}
 };
--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -5,3 +5,12 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseName, TCGop) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+
+	EXPECT_EQ(6, cal->getGop(2, 3));
+	EXPECT_EQ(0, cal->getGop(0, 0));
+	EXPECT_EQ(120, cal->getGop(12, 10));
+}
