943c42af7f21ef607164df15677300f949a685d1
tw2013.kim <tigger6392@gmail.com>
2023-06-01 09:27:26 +0900

    first commit

---
+++ Project1/calc.cpp
@@ -0,0 +1,4 @@
+class Cal
+{
+public:
+};
---
+++ Sample-Test1/calc_test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Project1/calc.cpp"
+
+TEST(TestCaseName, TestName) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+}
