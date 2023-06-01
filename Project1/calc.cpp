564798da7f31463542e1222d4b114795fb90b441
DanielKim <sshwas@gmail.com>
2023-06-01 09:57:48 +0900

    GetDivide, GetSumSum  dev.

--- Project1/calc.cpp
+++ Project1/calc.cpp
@@ -1,4 +1,14 @@
 class Cal
 {
 public:
+
+	int GetDivide(int a, int b)
+	{
+		return ( b ? a / b : 0 );
+	}
+
+	int GetSumSum(int a, int b, int c)
+	{
+		return a + b + c;
+	}
 };
--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -5,3 +5,21 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseName, TCDivide) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+
+	EXPECT_EQ(12, cal->GetDivide(24, 2));
+	EXPECT_EQ(0, cal->GetDivide(5, 0));
+	EXPECT_EQ(100, cal->GetDivide(1000, 10));
+}
+
+TEST(TestCaseName, TCSumSum) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+
+	EXPECT_EQ(6, cal->GetSumSum(1, 2, 3));
+	EXPECT_EQ(15, cal->GetSumSum(4, 5, 6));
+	EXPECT_EQ(24, cal->GetSumSum(7, 8, 9));
+}
