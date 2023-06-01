e3453a9f6d929de93d9087d62f5c90eb2952dd91
dawn <5ekdmsdldl@gmail.com>
2023-06-01 10:05:09 +0900

    제곱, 빼기 함수

--- Project1/calc.cpp
+++ Project1/calc.cpp
@@ -1,4 +1,12 @@
 class Cal
 {
 public:
+	int getZegop(int a)
+	{
+		return a * a;
+	}
+	int getMinus(int a, int b)
+	{
+		return a - b;
+	}
 };
--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -1,7 +1,20 @@
 #include "pch.h"
 #include "../Project1/calc.cpp"
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, TestZegop) {
 	Cal* cal = new Cal();
-	EXPECT_TRUE(cal != nullptr);
+	int res1 = cal->getZegop(0);
+	EXPECT_EQ(res1, 0);
+	int res2 = cal->getZegop(2);
+	EXPECT_EQ(res2, 4);
+}
+
+TEST(TestCaseName, TestMinus) {
+	Cal* cal = new Cal();
+	int res1 = cal->getMinus(1, 1);
+	EXPECT_EQ(res1 , 0);
+	int res2 = cal->getMinus(0, 1);
+	EXPECT_EQ(res2 , -1);
+	int res3 = cal->getMinus(1, 0);
+	EXPECT_EQ(res3 , 1);
 }
