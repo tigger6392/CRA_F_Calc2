d37fbc994a53b48e600e419cb7de79a2ab415e8b
tw2013.kim <tigger6392@gmail.com>
2023-06-01 09:59:53 +0900

    getSum() added
    Signed-off-by: tw2013.kim <tigger6392@gmail.com>

--- Sample-Test1/calc_test.cpp
+++ Sample-Test1/calc_test.cpp
@@ -5,3 +5,10 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseName, SumTestm) {
+	Cal* cal = new Cal();
+	int ret = cal->getSum(1, 2);
+	EXPECT_TRUE(ret == 3);
+	delete cal;
+}
